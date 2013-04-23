f5e33239dfa4335778ad5853953cd0c6cac05912
Eduard Carreras <ecarreras@gisce.net>
2013-04-23 12:03:22 +0200

    Add OOrqJob

--- oorq/oorq.py
+++ oorq/oorq.py
@@ -8,6 +8,12 @@ from rq import Worker, Queue
 from rq import push_connection
 
 
+def serialize_date(dt):
+    if not dt:
+        return False
+    return dt.strftime('%Y-%m-%d %H:%M:%S')
+
+
 class OorqBase(osv.osv):
     """OpenObject RQ Base.
     """
@@ -92,3 +98,50 @@ class OorqQueue(osv.osv):
         return queues
 
 OorqQueue()
+
+
+class OorqJob(osv.osv):
+    """OpenObject RQ Queue.
+    """
+    _name = 'oorq.job'
+    _inherit = 'oorq.base'
+    _auto = False
+
+    _columns = {
+        'jid': fields.char('Job Id', size=36),
+        'created_at': fields.datetime('Created at'),
+        'enqueued_at': fields.datetime('Enqueued at'),
+        'ended_at': fields.datetime('Ended at'),
+        'origin': fields.char('Origin', size=32),
+        'result': fields.text('Result'),
+        'exc_info': fields.text('Exception info'),
+        'description': fields.text('Description')
+    }
+
+    def read(self, cursor, uid, ids, fields=None, context=None):
+        """Show connected workers.
+        """
+        redis_conn = Redis()
+        push_connection(redis_conn)
+        if not context:
+            context = {}
+        if 'queue' in context:
+            queues = [Queue(context['queue'])]
+        else:
+            queues = Queue.all()
+        jobs = []
+        for qi, queue in enumerate(queues):
+            jobs += [dict(
+                id=int('%s%s' % (qi + 1, ji)),
+                jid=job.id,
+                created_at=serialize_date(job.created_at),
+                enqueued_at=serialize_date(job.enqueued_at),
+                ended_at=serialize_date(job.ended_at),
+                origin=job.origin or False,
+                result=job._result or False,
+                exc_info=job.exc_info or False,
+                description=job.description or False)
+                     for ji, job in enumerate(queue.jobs)]
+        return jobs
+
+OorqJob()
