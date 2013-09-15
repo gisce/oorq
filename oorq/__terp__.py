49135509bfd41cd20cd7774edfb483c3f67489c3
Eduard Carreras <ecarreras@gmail.com>
2013-09-15 16:19:52 +0200

    Bump to v2.1.0
    GH-18: Add a hash to the job to detect duplicates

--- oorq/__terp__.py
+++ oorq/__terp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "oorq",
-    "version": "2.0.0",
+    "version": "2.1.0",
     "depends": ["base"],
     "author": "Eduard Carreras",
     "category": "Base",
