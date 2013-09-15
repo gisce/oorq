e075c6f12378c4fa8657cb5dadcb81f03f83e50a
Eduard Carreras <ecarreras@gmail.com>
2013-09-15 13:37:51 +0200

    Fix compare to test_dsn first time

--- tests/test_monkeypatch.py
+++ tests/test_monkeypatch.py
@@ -8,14 +8,13 @@ class TestMonkeypatch(OORQTestCase):
         from tools import config
         for attr, value in self.conf.items():
             config[attr] = value
-        import sql_db
-        self.assertEqual(sql_db.dsn(self.conf['database']),
-                         'dbname=%s' % self.conf['database'])
-        monkeypatch_sql_db_dsn()
         test_dsn = ''
         for p in ('host', 'port', 'user', 'password'):
             cfg = self.conf.get('db_' + p, '')
             if cfg:
                 test_dsn += '%s=%s ' % (p, cfg)
         test_dsn += 'dbname=%s' % self.conf['database']
+        import sql_db
+        self.assertEqual(sql_db.dsn(self.conf['database']), test_dsn)
+        monkeypatch_sql_db_dsn()
         self.assertEqual(sql_db.dsn(self.conf['database']), test_dsn)
