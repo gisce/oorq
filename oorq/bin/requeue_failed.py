95dbb07b6f42972ac9242849917073d0bd79df0b
Eduard Carreras <ecarreras@gisce.net>
2013-11-20 18:25:27 +0100

    Added scripts

---
+++ oorq/bin/requeue_failed.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import sys
+import times
+from redis import from_url
+from rq import use_connection, get_failed_queue, requeue_job, Queue
+
+INTERVAL = 7200  # Seconds
+MAX_ATTEMPTS = 5
+PERMANENT_FAILED = 'permanent'
+
+redis_conn = from_url(sys.argv[1])
+use_connection(redis_conn)
+
+fq = get_failed_queue()
+pq = Queue(name=PERMANENT_FAILED)
+
+for job in fq.jobs:
+    job.meta.setdefault('attempts', 0)
+    if job.meta['attempts'] > MAX_ATTEMPTS:
+        print "Job %s %s attempts. MAX ATTEMPTS %s limit exceeded" % (
+                job.id, job.meta['attempts'], MAX_ATTEMPTS
+        )
+        print job.description
+        print job.exc_info
+        print
+        fq.remove(job)
+        pq.enqueue_job(job)
+        print "Moved to %s queue" % PERMANENT_FAILED
+    else:
+        ago = (times.now() - job.enqueued_at).seconds
+        print "%s: attemps: %s enqueued: %ss ago" % (job.id,
+                                                     job.meta['attempts'],
+                                                     ago),
+        if ago >= INTERVAL:
+            job.meta['attempts'] += 1
+            job.save()
+            print "(Requeue)"
+            requeue_job(job.id)
+        else:
+            print "(Waiting %ss)" % (INTERVAL - ago)
