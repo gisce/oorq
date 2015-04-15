1a8208da4fb35e20205d928980b6b484251b2b71
Guillem Julià Blasi <gjulia@gisce.net>
2015-04-15 13:35:19 +0200

    Always setup redis connection

--- oorq/__init__.py
+++ oorq/__init__.py
@@ -2,7 +2,9 @@
 """OpenObject RQ.
 """
 from rq import get_current_job
+from .oorq import setup_redis_connection
 
+setup_redis_connection()
 if get_current_job():
     # importing osv.osv it tries to parse args we need to remove, because
     # there are a conflict bettween rqworker and openerp-server args.
