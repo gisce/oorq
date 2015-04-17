2df836824d2dbf241ffb961de0349b91745daef0
Eduard Carreras <ecarreras@gisce.net>
2015-04-17 10:31:24 +0200

    Check connection first
    Conflicts:
    	oorq/__init__.py
    	oorq/ir_cron.py

--- oorq/__init__.py
+++ oorq/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf8 -*-
 """OpenObject RQ.
 """
-from rq import get_current_job
+from rq import get_current_connection, get_current_job
 
-if get_current_job():
+
+if get_current_connection() and get_current_job():
     # importing osv.osv it tries to parse args we need to remove, because
     # there are a conflict bettween rqworker and openerp-server args.
     import sys
