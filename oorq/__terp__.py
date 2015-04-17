278fb34fc8d8514f624ffe6db3b38dc06b38f77c
Eduard Carreras <ecarreras@gisce.net>
2015-04-17 10:39:04 +0200

    Bump to v1.5.5

--- oorq/__terp__.py
+++ oorq/__terp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "oorq",
-    "version": "1.5.4",
+    "version": "1.5.5",
     "depends": ["base"],
     "author": "Eduard Carreras",
     "category": "Base",
