3d60e52339d7aee772ffe563c333b015a5a017f9
Marc Roig <mroig@gisce.net>
2017-06-13 14:22:18 +0200

    Added message of how many workers

--- oorq/bin/create_burst_workers.sh
+++ oorq/bin/create_burst_workers.sh
@@ -3,6 +3,8 @@
 # Usage: ./create_burst_workers.sh #workers queues
 # Example: ./create_burst_workers.sh 5 sii import_xml
 
+echo "Creating $1 workers"
+
 for i in `seq 1 $1`;
 do
     rq worker --burst ${@:2} &
