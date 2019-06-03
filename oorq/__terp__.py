a66a18e61562d7947988f49c9ea3062eae6bb731
Eduard Carreras <ecarreras@gmail.com>
2019-06-03 11:49:35 +0200

    Bump to v1.16.0

--- oorq/__terp__.py
+++ oorq/__terp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "oorq",
-    "version": "1.15.1",
+    "version": "1.16.0",
     "depends": ["base"],
     "author": "Eduard Carreras",
     "category": "Base",
