e52a2df6cf63d5b1977b86815f65262752efdf19
Eduard Carreras <ecarreras@gisce.net>
2019-09-25 10:52:51 +0200

    Support for RQ v1.1

--- oorq/bin/requeue_failed.py
+++ oorq/bin/requeue_failed.py
@@ -2,7 +2,9 @@
 import sys
 import times
 from redis import from_url
-from rq import use_connection, get_failed_queue, requeue_job, Queue
+from rq import use_connection, requeue_job, Queue
+from rq.job import Job
+from rq.registry import FailedJobRegistry
 
 INTERVAL = 7200  # Seconds
 MAX_ATTEMPTS = 5
@@ -11,28 +13,29 @@ PERMANENT_FAILED = 'permanent'
 redis_conn = from_url(sys.argv[1])
 use_connection(redis_conn)
 
-fq = get_failed_queue()
+fq = FailedJobRegistry()
+pfq = FailedJobRegistry(PERMANENT_FAILED)
 pq = Queue(name=PERMANENT_FAILED)
 
-for job in fq.jobs:
+for job_id in fq.get_job_ids():
+    job = Job.fetch(job_id)
     job.meta.setdefault('attempts', 0)
     if job.meta['attempts'] > MAX_ATTEMPTS:
-        print "Job %s %s attempts. MAX ATTEMPTS %s limit exceeded on %s" % (
+        print("Job %s %s attempts. MAX ATTEMPTS %s limit exceeded on %s" % (
                 job.id, job.meta['attempts'], MAX_ATTEMPTS, job.origin
-        )
-        print job.description
-        print job.exc_info
-        print
+        ))
+        print(job.description)
+        print(job.exc_info)
+        print()
         fq.remove(job)
         pq.enqueue_job(job)
-        print "Moved to %s queue" % PERMANENT_FAILED
+        print("Moved to %s FailedJobRegistry" % PERMANENT_FAILED)
     else:
         ago = (times.now() - job.enqueued_at).seconds
         if ago >= INTERVAL:
-            print "%s: attemps: %s enqueued: %ss ago on %s (Requeue)" % (job.id,
-                                                     job.meta['attempts'],
-                                                     ago,
-                                                     job.origin)
+            print("%s: attemps: %s enqueued: %ss ago on %s (Requeue)" % (
+                job.id, job.meta['attempts'], ago, job.origin
+            ))
             job.meta['attempts'] += 1
             job.save()
-            requeue_job(job.id)
+            requeue_job(job.id, connection=redis_conn)
