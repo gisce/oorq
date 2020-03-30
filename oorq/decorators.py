6e1bfa9e6617fb4f37125555feac278014100458
Eduard Carreras <ecarreras@gisce.net>
2020-03-30 23:55:04 +0200

    use same code as `job` decorator

--- oorq/decorators.py
+++ oorq/decorators.py
@@ -190,41 +190,40 @@ class split_job(job):
                         conf_attrs, dbname, uid, osv_object, fname
                     ] + args[3:]
                     job_kwargs = kwargs
+                    at_front = self.at_front
                     if self.on_commit:
                         job = Job.create(
                             task,
-                            depends_on=current_job,
-                            result_ttl=self.result_ttl,
                             args=job_args,
-                            kwargs=job_kwargs
+                            kwargs=job_kwargs,
+                            timeout=self.timeout,
+                            result_ttl=self.result_ttl,
+                            depends_on=current_job
                         )
-                        set_hash_job(job)
                         transaction_id = id(cursor)
-                        ProcessJobs.add_job(transaction_id, job, q)
+                        ProcessJobs.add_job(transaction_id, job, q, at_front)
                         log('Created split job (%s/%s) on queue %s in %s mode '
                             '(id:%s): [%s] pool(%s).%s%s '
                             '(waiting to commit/rollback %s)' % (
                                 idx + 1, len(chunks), q.name, mode, job.id,
                                 dbname, osv_object, fname, tuple(args[2:]),
                                 transaction_id
-                            )
-                        )
+                        ))
                     else:
                         job = q.enqueue(
                             task,
                             depends_on=current_job,
                             result_ttl=self.result_ttl,
                             args=job_args,
-                            kwargs=job_kwargs
+                            kwargs=job_kwargs,
+                            at_front=at_front
                         )
-                        set_hash_job(job)
                         log('Created split job (%s/%s) on queue %s in %s mode '
                             '(id:%s): [%s] pool(%s).%s%s ' % (
                                 idx + 1, len(chunks), q.name, mode, job.id,
                                 dbname, osv_object, fname, tuple(args[2:])
-                            )
-                        )
-
+                        ))
+                    set_hash_job(job)
                     jobs.append(job)
                 return jobs
             else:
