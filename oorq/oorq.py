e84119ccd9ff306dae5268750d2bddae54f7e006
PSala <polsalalidon@gmail.com>
2024-11-21 13:54:41 +0100

    Return 0% if not joined

--- oorq/oorq.py
+++ oorq/oorq.py
@@ -93,6 +93,8 @@ class JobsPool(object):
 
     @property
     def progress(self):
+        if not self.joined:
+            return 0.0
         if not self.num_jobs:
             return 100.0
         return (len(self.done_jobs) * 1.0 / self.num_jobs) * 100
